4e6e970abee72da0e45eee1111d8054b5bb3ebae
lcranfrd <lcranfrd@comcast.net>
2020-12-20 09:25:51 -0500

    printQuote functionality added.

--- js/script.js
+++ js/script.js
@@ -10,19 +10,93 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: 'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.',
+    source: 'Albert Einstein'
+  },
+  {
+    quote: 'The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.',
+    source: 'Isac Asimov'
+  },
+  {
+    quote:'Never memorize something that you can look up',
+    source: 'Albert Einstein'
+  },
+  {
+    quote: 'Somewhere, something incredible is waiting to be known.',
+    source: 'Carl Sagan'
+  },
+  {
+    quote: 'Time is a drug. Too much of it kills you.',
+    source: 'Terry Prachett',
+    citation: 'Small Gods'
+  },
+  {
+    quote: 'I\'m sure the universe is full of intelligent life. It\'s just been too intelligent to come here.',
+    source: 'Arthur C. Clarke'
+  },
+  {
+    quote: 'An expert is a person who has made all the mistakes that can be made in a very narrow field.',
+    source: 'Niels Bohr'
+  },
+  {
+    quote: 'Science and religion are not at odds. Science is simply too young to understand.',
+    source:'Dan Brown',
+    citation: 'Angels & Demons'
+  },
+  {
+    quote: 'That which can be asserted without evidence, can be dismissed without evidence',
+    source:'Christopher Hitchens'
+  },
+  {
+    quote: 'Everything must be made as simple as possible. But not simpler.',
+    source:'Albert Einstein'
+  },
+  {
+    quote: 'We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.',
+    source:'Stephen Hawking'
+  },
+  {
+    quote: 'If we knew what it was we were doing, it would not be called research, would it?',
+    source:'Albert Einstein'
+  },
+  {
+    quote: 'Your assumptions are your windows on the world. Scrub them off every once in a while, or the light won\'t come in.',
+    source:'Isaac Asimov'
+  },
+  {
+    quote: 'Space is big. You just won\'t believe how vastly, hugely, mind-bogglingly big it is. I mean, you may think it\'s a long way down the road to the chemist\'s, but that\'s just peanuts to space.',
+    source:'Douglas Adams',
+    citation: 'TheHitchhiker\s Guide to the Galaxy'
+  },
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote() {
+   const genRandomNum = Math.floor(Math.random() * quotes.length);
+   return quotes[genRandomNum];
+}
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote() {
+  const newQuote = getRandomQuote();
+  let htmlStr = `<p class="quote">${newQuote.quote}</p><p class="source">${newQuote.source}`;
+  if(typeof newQuote.citation !== 'undefined'){
+    htmlStr += `<span class="citation">${newQuote.citation}</span>`;
+  }
+  if(typeof newQuote.year !== 'undefined'){
+    htmlStr += `<span class="year">${newQuote.year}</span>`;
+  }
+  htmlStr += '</p>';
+  document.getElementById('quote-box').innerHTML = htmlStr;
+}
 
 
 /***
